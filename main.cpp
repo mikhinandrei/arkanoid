7eef325463530158b2e6a5295218083868b3e2e6
mikhinandrei <mikhinandrei@yandex.ru>
2016-11-13 20:12:04 +0300

    binary_tree

--- main.cpp
+++ main.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-struct find_parent_function_return{
-    Node* parent;
-    char direction;
-};
-
-class Node{
-    public:
-    Node(int);
-    Node *left;
-    Node *right;
+struct Node{
+    Node *left = NULL;
+    Node *right = NULL;
     int value;
 };
 
 class Binary_tree{
-    public:
-    int find(int);
+public:
+    bool find(int);
     void add(int);
-    bool contains(int);
-    bool remove(Node*, int);
-    void out();
-    Node *head;
+    bool remove(int);
+    void out(Node*);
+    Node *head = nullptr;
 
-    private:
+private:
     void insert(Node*, int);
-    find_parent_function_return find_parent(Node*);
+    Node* find_with_parent(int, Node*&);
     Node* find_left(Node*);
 };
 
@@ -34,11 +27,13 @@ int main() {
     char answer, choose;
     int n;
 
+    setlocale(LC_ALL,"Russian");
+
     Binary_tree tree;
-    cout << "?????? ???-?????? ???????? ? ???????? (? - ??, ????????? - ???): ";
+    cout << "Хотите что-нибудь сделать с деревом? (y - да, остальное - нет):  ";
     cin >> answer;
-    while (answer == '?'){
-        cout << "1-????????, 2-???????, 3-?????, 4-?????: ";
+    while (answer == 'y'){
+        cout << "1-вставить, 2-удалить, 3-найти, 4-вывод: ";
         cin >> choose;
         switch (choose) {
             case '1':
@@ -47,7 +42,7 @@ int main() {
                 break;
             case '2':
                 cin >> n;
-                tree.remove(tree.head, n);
+                tree.remove(n);
                 break;
             case '3':
                 cin >> n;
@@ -59,25 +54,25 @@ int main() {
                 }
                 break;
             case '4':
-                tree.out();
+                tree.out(tree.head);
                 break;
             default:
                 cout << "Error";
                 break;
         }
-        cout << "?????? ??????????? (? - ??, ????????? - ???): ";
+        cout << "Хотите продолжить? (д - да, остальное - нет): ";
         cin >> answer;
     }
     return 0;
 }
 
-Node::Node(int val){
-    value = val;
-}
-
 void Binary_tree::add(int val){
-    if (head == NULL){
+    if (!(head)){
+        Node *p;
+        head = p;
+        delete(p);
         head->value = val;
+        head->right = nullptr;
     }
     else{
         insert(head, val);
@@ -86,16 +81,18 @@ void Binary_tree::add(int val){
 
 void Binary_tree::insert(Node *node, int val){
     if (val < node->value){
-        if (node->left == NULL) {
-            node->left = new Node(val);
+        if (node->left == nullptr) {
+            node->left = new Node;
+            node->left->value = val;
         }
         else{
             insert(node->left, val);
         }
     }
     else{
-        if (node->right == NULL){
-            node->right = new Node(val);
+        if (node->right == nullptr){
+            node->right = new Node;
+            node->right->value = val;
         }
         else{
             insert(node->right, val);
@@ -103,54 +100,116 @@ void Binary_tree::insert(Node *node, int val){
     }
 }
 
-bool Binary_tree::remove(Node *node, int val){
-    if (node == NULL){
+bool Binary_tree::remove(int val){
+    Node *current = nullptr;
+    Node *parent;
+
+    current = find_with_parent(val, *&parent);
+
+    if (current == nullptr){
         return false;
     }
-    if (val > node->value){
-        remove(node->right, val);
-    }
-    else{
-        if (val < node->value){
-            remove(node->left, val);
+    //Если у удаляемого нет ребёнка справа, то его левый ребёнок идёт вверх
+    if(current->right == nullptr){
+        if (parent == nullptr){
+            head = current->left;
         }
-        else {  // val == node->value
-            find_parent_function_return found = find_parent(node);
-            Node *n;
-            switch (found.direction){
-                case 'l':
-                    n = found.parent->left;
-                    break;
-                case 'r':
-                    n = found.parent->right;
-                    break;
+        else{
+            if (parent->value > current->value) {
+                parent->left = current->left;
             }
-            if ((node->left == NULL) && (node->right == NULL)){
-                delete(node);
-                n = NULL;
-                return true;
+            else {
+                parent->right = current->left;
             }
-            if (node->left == NULL){
-                n = node->right;
-                delete(node);
-                return true;
+        }
+        return true;
+    }
+    //Есть правый ребёнок без левого ребёнка
+    if (current->right->left == nullptr){
+        current->right->left = current->left;
+        if (parent == nullptr){
+            head = current->right;
+        }
+        else{
+            if (parent->value > current->value) {
+                parent->left = current->right;
             }
-            if (node->right == NULL){
-                n = node->left;
-                delete(node);
-                return true;
+            else {
+                parent->right = current->right;
             }
-            if (node->right->left == NULL){
-                node->value = node->right->value;
-                node->right = node->right->right;
-                return true;
-            }
-            else{
-                Node *node1 = find_left(node->right);
-                node->value = node1->value;
-                find_parent_function_return found = find_parent(node1);
-                found.parent->left = NULL;
-            }
+        }
+        return true;
+    }
+    else {
+        Node* leftmost = find_left(current->right);
+        if (parent == nullptr){
+            head->left = leftmost;
+        }
+        else{
+            current->value = leftmost->value;
+            Node *parent_leftmost;
+            leftmost = find_with_parent(val, *&parent_leftmost);
+            parent_leftmost->left = nullptr;
         }
     }
 }
+
+Node* Binary_tree::find_with_parent(int val, Node*& parent) {
+    Node* current = head;
+
+    while(current != nullptr){
+        if (val < current->value){
+            parent = current;
+            current = current->left;
+        }
+        else{
+            if (val > current->value){
+                parent = current;
+                current = current->right;
+            }
+            else{
+                break;
+            }
+        }
+    }
+    return current;
+}
+
+bool Binary_tree::find(int val){
+    Node *current = head;
+    while ((current->value != val) && (current != nullptr)){
+        cout << current->value << " -> ";
+        if (val < current->value){
+            current = current->left;
+        }
+        else{
+            current = current->right;
+        }
+    }
+    if (current == nullptr){
+        return false;
+    }
+    cout << current->value << endl;
+    return true;
+};
+
+void Binary_tree::out(Node* node){
+    if (node){
+        cout << node->value << " ";
+    }
+    if (node->left){
+        out(node->left);
+    }
+    if (node->right){
+        out(node->right);
+    }
+};
+
+Node* Binary_tree::find_left(Node *node){
+    if (node->left){
+        return find_left(node->left);
+    }
+    else {
+        return node;
+    }
+};
